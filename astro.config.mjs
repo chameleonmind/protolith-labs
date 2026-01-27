27e9e4eac95dfeb1245267cbb9df5d5de876d3d9
Milos Milosevic <highlander.mm@gmail.com>
2026-01-27 19:09:01 +0100

    Add analytics. Change privacy policy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()],
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
